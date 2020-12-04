e7b2ef11538f00a01e3496f9a1e62576cd048aef
albertosoriano <onairos.95@gmail.com>
2020-12-04 12:26:22 +0100

    metodo GLIDE para una imagen circular en el MAIN

--- app/src/main/java/com/dam/mynicestart10/Activity2.java
+++ app/src/main/java/com/dam/mynicestart10/Activity2.java
@@ -5,13 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class Activity2 extends AppCompatActivity {
+
+    ImageView cara1;
+    Button btnMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        cara1 = findViewById(R.id.imageView2);
+        btnMain = findViewById(R.id.button4);
+        Glide.with(this)
+                .load(R.drawable.cara)
+                .centerCrop()
+                .circleCrop()
+                .into(cara1);
+
+
     }
 
     public void acceder2(View view) {
